0b0ba0220077e79089ddb54414c25fdd34357dc1
Marcelo Eltz <marcelo@growdev.com.br>
2020-12-15 21:25:44 -0300

    Adicionado o Grid na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Paper from "@material-ui/core/Paper";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // flexGrow: 1,
+    height: "100vh",
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      direction="row"
+      className={classes.root}
+      style={{ background: "#333" }}
+    >
+      <Grid item xs={12} md={6} style={{ background: "red" }}>
+        teste
+      </Grid>
+      <Grid item xs={12} md={6} style={{ background: "blue" }}>
+        teste
+      </Grid>
+      <Grid item xs={12} style={{ background: "black" }}>
+        teste
+      </Grid>
+    </Grid>
   );
 }
-
-export default App;
