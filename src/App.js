6bbcf5b9d7f782b0793f3e52103e688b9419507f
Marcelo Eltz <marcelo@growdev.com.br>
2020-12-15 21:30:17 -0300

    Alterado o conteudo do ultimo grid

--- src/App.js
+++ src/App.js
@@ -37,8 +37,25 @@ export default function App() {
       <Grid item xs={12} md={6} style={{ background: "blue" }}>
         teste
       </Grid>
-      <Grid item xs={12} style={{ background: "black" }}>
-        teste
+      <Grid item xs={12} style={{ background: "black", color: "white" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac
+        finibus sapien. Donec sit amet enim congue, venenatis arcu posuere,
+        faucibus nulla. Sed nec ante non orci tincidunt efficitur commodo
+        feugiat elit. Vivamus convallis a sem a sagittis. Etiam volutpat lacinia
+        risus sed interdum. Donec gravida lorem neque, vel mattis nisi finibus
+        quis. Vestibulum pulvinar ligula scelerisque metus accumsan, quis
+        pellentesque odio luctus. Nulla iaculis mauris ut aliquet imperdiet.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Pellentesque est ligula, dapibus quis dapibus
+        nec, aliquet a nisi. Vivamus erat arcu, pretium pretium interdum ac,
+        tempor a dolor. Aenean congue mi tellus, sed consequat nibh tristique
+        vitae. Suspendisse nec ex sed nibh volutpat commodo in id massa. Donec
+        dictum ultricies convallis. Maecenas ac porta enim. Cras a egestas nisl,
+        sit amet tempor ex. Maecenas ac dapibus elit. Etiam fermentum sapien
+        purus, dignissim dictum mi pellentesque ut. Phasellus quis varius leo, a
+        vulputate tellus. Donec nec malesuada ante. Integer eu blandit dui, sit
+        amet pellentesque mauris. Cras ut pulvinar elit, ut tincidunt nunc.
+        Mauris ut malesuada est. Nullam iaculis pretium augue sit amet lacinia.
       </Grid>
     </Grid>
   );
